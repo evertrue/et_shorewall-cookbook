3f5bc83cc310ce598fa51a6817af5787a7f396a9
Eric Herot <eric.github@herot.com>
2013-10-22 16:08:53 -0400

    First rev

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'shorewall'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'eric.herot@evertrue.com'
+license          'All rights reserved'
+description      'Installs/Configures et_shorewall'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: et_shorewall
+# Recipe:: default
+#
+# Copyright (C) 2013 EverTrue, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{
+  shorewall
+  make
+}.each do |pkg|
+  package pkg
+end
+
+cookbook_file "/etc/default/shorewall" do
+  source "default"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+execute "shorewall_make" do
+  cwd node["shorewall"]["conf_dir"]
+  command "make"
+  action :nothing
+end
+
+%w{
+  interfaces
+  masq
+  policy
+  routestopped
+  rules
+  zones
+}.each do |conf_file|
+
+  template "#{node['shorewall']['conf_dir']}/#{conf_file}" do
+    owner "root"
+    group "root"
+    mode "0644"
+    source "#{conf_file}.erb"
+    notifies :run, "execute[shorewall_make]"
+  end
+
+end
+
+service "shorewall" do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
+end
